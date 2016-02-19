b15c43644bd0aa91e8f744f2c3f90ab65fe39bf6
Matt Langlois <fletchto99@gmail.com>
2016-02-18 21:20:15 -0500

    Some documentation on the C side

--- PebbleKit Js Demo/src/main.c
+++ PebbleKit Js Demo/src/main.c
@@ -1,20 +1,25 @@
 #include <pebble.h>
 
+//Some vars used throughout the app
 static Window *window;
 static TextLayer *text_layer;
+static bool active = false;
+static GColor color = GCOlorWhite;
 
-static bool   active;
-static GColor color;
-
-
+/*
+ * Initializes the main window and adds the text layer which will contain the quote
+ */
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  //Setup the text layer
   text_layer = text_layer_create(GRect(0, 30, bounds.size.w, 120));
   text_layer_set_text(text_layer, "Loading...");
   text_layer_set_background_color(text_layer, GColorClear);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+
+  //Add the text layer to the root window layer
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
@@ -25,22 +30,26 @@ static void window_unload(Window *window) {
 static void in_received_handler(DictionaryIterator *iter, void *context) {
   Tuple *tuple = NULL;
 
+  //Look for the background toggle
   tuple = dict_find(iter, 0);
   if(tuple) {
     active = tuple->value->int32;
   }
 
+  //look for the color to set the background
   tuple = dict_find(iter, 1);
   if(tuple) {
     color = GColorFromHEX(tuple->value->int32);
   }
 
+  //Apply the background color if it was changed and enabled
   if(active) {
     window_set_background_color(window, color);
   } else {
     window_set_background_color(window,GColorWhite);
   }
 
+  //Update the quote text
   tuple = dict_find(iter, 2);
   if (tuple) {
     text_layer_set_text(text_layer, tuple->value->cstring);
@@ -48,11 +57,13 @@ static void in_received_handler(DictionaryIterator *iter, void *context) {
 
 }
 
+/*
+ * Initializes the app:
+ * - Listen for app messages
+ * - Initialize the main window
+ */
 static void init(void) {
-
-  active  = false;
-  color   = GColorWhite;
-
+  //Listen for incoming app messages
   app_message_open(256, 0);
   app_message_register_inbox_received(in_received_handler);
 
@@ -61,6 +72,8 @@ static void init(void) {
     .load = window_load,
     .unload = window_unload,
   });
+
+  //push the main window to the window stack
   window_stack_push(window, true);
 }
 
